57da8e9f13fb418ba4a0992c900d34239bdf8de0
Call me...leo <74614943+its-leo-bitch@users.noreply.github.com>
2020-12-21 13:28:12 +0530

    Torrent search engine Bitch📡

--- userbot/Plugins/torrent_search.py
+++ userbot/Plugins/torrent_search.py
@@ -2,7 +2,7 @@
 Torrent Search Plugin for Userbot. //torrentdownloads.me
 cmd: .search search_string
 Note: Number of results are currently limited to 11
-By:-@Zero_cool7870
+By:-@betalinktobot
 
 """
 from bs4 import BeautifulSoup as bs 
