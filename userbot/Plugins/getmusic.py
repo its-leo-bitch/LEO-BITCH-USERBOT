d5a01dc051b9812b64a81aa1a67ee17f1471acda
Call me...leo <74614943+its-leo-bitch@users.noreply.github.com>
2020-12-21 12:00:45 +0530

    Bitch songs

--- userbot/Plugins/getmusic.py
+++ userbot/Plugins/getmusic.py
@@ -33,7 +33,7 @@ async def _(event):
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    await event.edit("Ok finding Your Awesome Song From My Database ◉‿◉")    
+    await event.edit("Ok finding Your Awesome Song From My Bitch Database ◉‿◉")    
     bruh(str(cmd))
     l = glob.glob("*.mp3")
     loa = l[0]
